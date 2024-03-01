dc7048b683d11e7df3ead2b09451ad5fb1380ef4
Dominik Josipović <dominik.josipovic@gmail.com>
2024-03-01 12:49:08 +0000

    First commit

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser');
+const dns = require('dns');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -18,6 +21,29 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+let storage = [];
+app.post('/api/shorturl', function(req, res) {
+  let url = req.body.url;
+  dns.lookup(url, (err, address) => {
+    if (err) res.json({ error: 'invalid url' });
+    else {
+      let shortUrl = storage.length + 1;
+      let originalUrl = url;
+      let add = { id: shortUrl, addr: address}
+      storage.push(add);
+      res.json({ original_url : originalUrl, short_url : shortUrl});
+    }
+  });
+});
+app.get('/api/shorturl/:short_url', function(req, res) {
+  let short_url = parseInt(req.params.short_url);
+  let url = storage.find(x => x.id == short_url);
+  let address = url.addr
+  res.redirect(address);
+  console.log(url);
+});
+
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
